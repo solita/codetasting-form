312c8cd4f9d3b0b6deb228f43d0d51aaaac8984e
Esko Luontola <esko.luontola@solita.fi>
2013-09-18 15:43:23 +0300

    Translated and updated for Solita Dojo

--- ilmo.py
+++ ilmo.py
@@ -8,27 +8,29 @@ SMTP_SERVER = "mail-host-here"
 SMTP_PORT = 587
 
 EMAIL_FROM = "no-reply@solita.fi"
-EMAIL_SUBJECT = "Solita Code Tasting ilmoittautuminen vastaanotettu"
-EMAIL_SPACE = ", "
+EMAIL_SUBJECT = "Solita Dojo registration received"
 
 ##Write form to file
 form = cgi.FieldStorage()
 name = form.getvalue('name')
+company = form.getvalue('company')
 email = form.getvalue('email')
-phone = form.getvalue('phone')
-background = form.getvalue('background')
-objectives = form.getvalue('objectives')
 allergies = form.getvalue('allergies')
-data = strftime("%Y-%m-%d-%H-%M", gmtime()) + ";" + name + ";" + email + ";" + phone + ";"  + background + ";" + objectives + ";" + allergies + "\n"
+data = strftime("%Y-%m-%d-%H-%M", gmtime()) + ";" + name + ";" + company + ";" + email + ";" + allergies + "\n"
 f = open('~/ilmot.txt','a')
 f.write(data)
 f.close()
 
-msg = MIMEText("Nimi: " + name + "\n" + "Email: " + email + "\n" + "Puhelinnumero: " + phone + "\n" + "Tausta: " + background + "\n" "Tavoitteet: " + objectives + "\n" + "Erityisruokavalio " + allergies + "\n" + "\n" + "Kiitos ilmoittautumisestasi! Infoamme osallistujien valinnasta ilmoittautuneille perjantaina 16.11.2012. \n \n t. Solita Code Tasting Team")
+msg = MIMEText("Name: " + name + "\n" +
+    "Company: " + company + "\n" +
+    "Email: " + email + "\n" +
+    "Allergies " + allergies + "\n" +
+    "\n" +
+    "See you soon!\n\n - Solita Developers")
 msg['Subject'] = EMAIL_SUBJECT
-msg['To'] = EMAIL_SPACE.join(EMAIL_TO)
+msg['To'] = email
 msg['From'] = EMAIL_FROM
 msg.set_charset('utf-8')
 mail = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-mail.sendmail(EMAIL_FROM, EMAIL_TO, msg.as_string())
+mail.sendmail(EMAIL_FROM, email, msg.as_string())
 mail.quit()
