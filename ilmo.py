21c64baa03e002b24d836ee11b56002d9e2476c2
Esko Luontola <esko.luontola@solita.fi>
2013-09-23 16:24:56 +0300

    Fixes to sending mail

--- ilmo.py
+++ ilmo.py
@@ -4,10 +4,11 @@ from email.mime.text import MIMEText
 import cgi, cgitb, smtplib
 from time import gmtime, strftime
 
-SMTP_SERVER = "mail-host-here"
-SMTP_PORT = 587
+SMTP_SERVER = "mail.example.com"
+SMTP_PORT = 25
 
-EMAIL_FROM = "no-reply@solita.fi"
+EMAIL_FROM = "no-reply@example.com"
+EMAIL_BCC = "send-a-copy-here@example.com"
 EMAIL_SUBJECT = "Solita Dojo registration received"
 
 ##Write form to file
@@ -17,20 +18,23 @@ company = form.getvalue('company')
 email = form.getvalue('email')
 allergies = form.getvalue('allergies')
 data = strftime("%Y-%m-%d-%H-%M", gmtime()) + ";" + name + ";" + company + ";" + email + ";" + allergies + "\n"
-f = open('~/ilmot.txt','a')
+f = open('/tmp/path-to-ilmo.txt','a')
 f.write(data)
 f.close()
 
 msg = MIMEText("Name: " + name + "\n" +
     "Company: " + company + "\n" +
     "Email: " + email + "\n" +
-    "Allergies " + allergies + "\n" +
+    "Allergies: " + allergies + "\n" +
     "\n" +
-    "See you soon!\n\n - Solita Developers")
+    "Thank you for registration. See you soon!\n\n - Solita Developers")
 msg['Subject'] = EMAIL_SUBJECT
 msg['To'] = email
 msg['From'] = EMAIL_FROM
 msg.set_charset('utf-8')
 mail = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
-mail.sendmail(EMAIL_FROM, email, msg.as_string())
+mail.sendmail(EMAIL_FROM, [email, EMAIL_BCC], msg.as_string())
 mail.quit()
+
+print "Content-Type: text/plain;charset=utf-8"
+print
